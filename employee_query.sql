f13a462a6dbc0e8ce29ed548ce2cd456e3563210
matthewjgarcia <matthewjggarcia@gmail.com>
2023-12-04 18:47:09 -0800

    Added rest of sql statments

--- employee_query.sql
+++ employee_query.sql
@@ -18,3 +18,42 @@ join "Departments" as d
 on dman.dept_no = d.dept_no
 join "employees" as e
 on dman.emp_no = e.emp_no;
+
+--4.
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from "employees" as e
+join "dept_emp" as de
+on e.emp_no = de.emp_no
+join "Departments" as d
+on de.dept_no = d.dept_no;
+
+--5.
+select e.first_name, e.last_name, e.sex
+from "employees" as e
+where e.first_name = 'Hercules'
+and e.last_name like 'B%';
+
+--6.
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from "employees" as e
+join "dept_emp" as de
+on e.emp_no = de.emp_no
+join "Departments" as d
+on de.dept_no = d.dept_no
+where d.dept_name = 'Sales';
+
+--7.
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from "employees" as e
+join "dept_emp" as de
+on e.emp_no = de.emp_no
+join "Departments" as d
+on de.dept_no = d.dept_no
+where d.dept_name = 'Sales'
+or d.dept_name = 'Development';
+
+--8.
+select e.last_name, count(e.last_name) as freq
+from "employees" as e
+group by e.last_name
+order by freq desc;
